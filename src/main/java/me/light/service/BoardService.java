8e20ea3fc4ade4b31fc38e2f2c85b9043dca834a
ksangheon <ksangheon@gmail.com>
2022-06-08 15:13:58 +0900

    5rd 서비스 계층 구축

---
+++ src/main/java/me/light/service/BoardService.java
@@ -0,0 +1,17 @@
+package me.light.service;
+
+import java.util.List;
+
+import me.light.model.Board;
+
+public interface BoardService {
+	List<Board> getList();
+
+	Board get(Long bno);
+
+	void insert(Board board);
+
+	void update(Board board);
+
+	void delete(Long bno);
+}
---
+++ src/main/java/me/light/service/BoardServiceImpl.java
@@ -0,0 +1,45 @@
+package me.light.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import me.light.mapper.BoardMapper;
+import me.light.model.Board;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+	@Autowired
+	private BoardMapper boardMapper;
+
+	@Override
+	public List<Board> getList() {
+		return boardMapper.getList();
+	}
+
+	@Override
+	public Board get(Long bno) {
+		return boardMapper.get(bno);
+	}
+
+	@Override
+	public void insert(Board board) {
+		boardMapper.insert(board);
+
+	}
+
+	@Override
+	public void update(Board board) {
+		boardMapper.update(board);
+
+	}
+
+	@Override
+	public void delete(Long bno) {
+		boardMapper.delete(bno);
+
+	}
+
+}
