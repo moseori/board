beae95a57af2106282e75c7cc56dc1919d123e96
ksangheon <ksangheon@gmail.com>
2022-07-05 16:21:00 +0900

    2022-07-05 4. 잘못 업로드된 파일 정리

--- src/main/java/me/light/task/FileCheckTask.java
+++ src/main/java/me/light/task/FileCheckTask.java
@@ -1,14 +1,56 @@
 package me.light.task;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Component	
+import me.light.mapper.BoardAttachMapper;
+import me.light.model.BoardAttachVO;
+
+@Component
 public class FileCheckTask {
-	@Scheduled(cron="0 * * * * *")
+
+	@Autowired
+	private BoardAttachMapper attachMapper;
+
+	@Scheduled(cron = "0/10 * * * * *")
 	public void checkFile() {
-		System.out.println("FileCheckTask 실행중....");
-		
+		System.out.println("Check files ....");
+
+		List<BoardAttachVO> fileList = attachMapper.getOldFiles();
+
+		List<Path> fileListPaths = fileList
+				.stream()
+				.map(vo -> Paths.get("C:\\storage", vo.getUploadPath(), vo.getUuid() + "_" + vo.getFileName()))
+				.collect(Collectors.toList());
+
+		fileList.stream().filter(vo -> vo.isFileType() == true)
+				.map(vo -> Paths.get("C:\\storage", vo.getUploadPath(), "s_" + vo.getUuid() + "_" + vo.getFileName()))
+				.forEach(p -> fileListPaths.add(p));
+
+		File targetDir = Paths.get("c:/storage", getFolderToday()).toFile();
+		File[] removeFiles = targetDir.listFiles(file -> fileListPaths.contains(file.toPath()) == false);
+
+		Arrays.asList(removeFiles).stream().forEach(f -> f.delete());
+	}
+
+	private String getFolderToday() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar cal = Calendar.getInstance();
+		// cal.add(Calendar.DATE, -1);
+		return sdf.format(cal.getTime()).replace("-", File.separator);
 	}
 
 }
