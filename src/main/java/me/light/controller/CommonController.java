bab9ccd739760d5e70d3b9144fa4a98802adbdb5
ksangheon <ksangheon@gmail.com>
2022-07-07 09:53:12 +0900

    2022-07-07 1. Spring security 로그인 설정

---
+++ src/main/java/me/light/controller/CommonController.java
@@ -0,0 +1,18 @@
+package me.light.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CommonController {
+
+	@GetMapping("/accessError")
+	public String accessDenied() {
+		return "error/accessDenied";
+	}
+
+	@GetMapping("/customLogin")
+	public String loginForm() {
+		return "member/login";
+	}
+}
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -3,5 +3,8 @@
 <%@ include file="layout/header.jsp"%>
 <div class="container">
 	<h3>HOME</h3>
+	<p><a href="${contextPath}/security/all">모든 방문자 허용</a></p>
+	<p><a href="${contextPath}/security/member">회원등급 이상</a></p>
+	<p><a href="${contextPath}/security/admin">관리자 전용</a></p>
 </div>
 <%@ include file="layout/footer.jsp"%>
