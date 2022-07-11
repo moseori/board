4f10dde8eb189c33200cb34707402c941aaa6460
ksangheon <ksangheon@gmail.com>
2022-07-11 09:50:47 +0900

    2022-07-11 1. 스프링 시큐리티 로그인(커스텀 데이터 베이스 사용)

---
+++ src/test/java/me/light/security/MemberTest.java
@@ -0,0 +1,40 @@
+package me.light.security;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import me.light.AppTest;
+
+public class MemberTest extends AppTest{
+
+	@Autowired
+	DataSource dataSource;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Test
+	public void MemberInsertTest() {
+		String sql="insert into member_tbl(userId, userPw, userName) values(?,?,?)";
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		try {
+			conn=dataSource.getConnection();
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, "leekanghyup");
+			pstmt.setString(2, passwordEncoder.encode("1234"));
+			pstmt.setString(3, "이광협");
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
